8650df9fc93cabf90d711ce1d7e7fb3d29a0009a
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2024-09-23 14:55:28 -0300

    reutilzando codigo inicial
    aprimorar esse jogo que foi um projeto do curso web moderno do leonardo Leitão

---
+++ flappy.js
@@ -0,0 +1,246 @@
+const novoElemento = (classeName,tagName) => {
+    const elemento = document.createElement(tagName)
+    elemento.classList.add(classeName)
+
+    return elemento
+}
+
+const barreira = (reversa = false) => {
+    const elemento = novoElemento("barreira", "div")
+
+    const borda = novoElemento("borda","div")
+    const corpo = novoElemento("corpo", "div")
+
+    elemento.appendChild(reversa ? corpo : borda)
+    elemento.appendChild(reversa ? borda : corpo)
+
+    const setAltura = altura => corpo.style.height = `${altura}px`
+
+    return {
+        elemento,
+        setAltura
+    }
+}
+
+
+
+const parDeBarreiras = (altura, abertura, x) => {
+    const elemento = novoElemento("par-de-Barreiras", "div")
+
+    const superior =  barreira(true)
+    const inferior =  barreira(false)
+
+    elemento.appendChild(superior.elemento)
+    elemento.appendChild(inferior.elemento)
+
+    const sortearAbeertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+
+        superior.setAltura(alturaSuperior)
+        inferior.setAltura(alturaInferior)
+    }
+
+    const getX = () => parseInt(elemento.style.left.split("px")[0])
+    const setX = x => elemento.style.left = `${x}px`
+    const getLargura = () => elemento.clientWidth
+
+    sortearAbeertura()
+    setX(x)
+
+    return {
+        elemento,
+        superior,
+        inferior,
+        getX,
+        setX,
+        getLargura,
+        sortearAbeertura
+
+    }
+}
+
+
+
+function barreiras(altura, largura, abertura, espaco, notificarPonto){
+    this.pares = [
+        parDeBarreiras(altura, abertura, largura),
+        parDeBarreiras(altura, abertura, largura + espaco),
+        parDeBarreiras(altura, abertura, largura + espaco * 2),
+        parDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
+
+    const movimentoGradual = 4
+    this.animarLoop = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - movimentoGradual)
+
+            
+            if(par.getX() < par.getLargura()){
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbeertura()
+            }
+
+            const meioDaTela = largura / 2
+            const atravessandoMeio = par.getX() + movimentoGradual >= meioDaTela && par.getX() < meioDaTela
+
+            if(atravessandoMeio){
+                notificarPonto()
+            }
+        })
+
+        return this;
+    }
+}
+
+const personagem = (alturaDoJogo) => {
+    let voandoParaCima = false
+    let voandoParaBaixo = false
+
+    const elemento = novoElemento("personagemImg", "img")
+    elemento.src = "../imgs/passaro.png"
+
+    const getY = () => parseInt(elemento.style.bottom.split("px")[0])
+    const setY = x => elemento.style.bottom = `${x}px`
+
+    window.onkeydown = (evento) => {
+        if (evento.key === "w" || evento.key === "W") {
+            voandoParaCima = true
+        }
+        if (evento.key === "s" || evento.key === "S") {
+            voandoParaBaixo = true
+        }
+    }
+
+    window.onkeyup = (evento) => {
+        if (evento.key === "w" || evento.key === "W") {
+            voandoParaCima = false
+        }
+        if (evento.key === "s" || evento.key === "S") {
+            voandoParaBaixo = false
+        }
+    }
+
+    const  animarPersonagem = () => {
+        
+        let novoY = getY()
+
+        if (voandoParaCima) {
+            novoY += 8 
+        }
+        if (voandoParaBaixo) {
+            novoY -= 5 
+        }
+        const alturaMaxima = alturaDoJogo - elemento.clientWidth
+
+        if(novoY < 0){
+            setY(0)
+        }
+        else if(novoY >= alturaMaxima){
+            setY(alturaMaxima)
+        }
+        else{
+            setY(novoY)
+        }
+    }
+
+    setY(alturaDoJogo / 2)
+
+
+    return {
+        elemento,
+        getY,
+        setY,
+        animarPersonagem
+    }
+}
+
+
+
+const progressoGame = () => {
+    const elemento = novoElemento("progresso", "span")
+
+    const atualizarPontos = (pontos) => {
+        elemento.innerHTML = pontos
+    }
+    
+    atualizarPontos(0)
+    return {
+        elemento,
+        atualizarPontos
+    }
+    
+}
+
+const sobreposicaoSouN = (elemA, elemB) => {
+    const a = elemA.getBoundingClientRect()
+    const b = elemB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left && b.left +b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top +b.height >= a.top
+
+     
+
+    return horizontal && vertical
+    
+}
+
+
+const colisaoTest = (personagem, barreiras) => {
+    return barreiras.pares.some(parBarreiras => {
+        const superior = parBarreiras.superior.elemento;
+        const inferior = parBarreiras.inferior.elemento;
+        
+        return sobreposicaoSouN(personagem.elemento, superior) || sobreposicaoSouN(personagem.elemento, inferior);
+    });
+}
+
+
+const flappyBird = () => {
+    let pontos = 0
+
+    const areaGame = document.querySelector("[wm-flappy]")
+    const altura = areaGame.clientHeight
+    const largura = areaGame.clientWidth
+
+    const progresso = progressoGame()
+    const barreirasS = new barreiras(altura, largura, 200, 400, () => progresso.atualizarPontos(++pontos))
+    const persona = personagem(altura)
+
+    areaGame.appendChild(progresso.elemento)
+    areaGame.appendChild(persona.elemento)
+
+    barreirasS.pares.forEach(p => areaGame.appendChild(p.elemento))
+
+    let jogoParado = false;  // Flag para saber se o jogo está rodando ou parado dando trabalho demais
+    let temporizadorJogo;
+
+    const inicioDeJogo = () => {
+        
+        temporizadorJogo = setInterval(() => {
+            if (!jogoParado) {
+                // Atualiza as animações das barreiras e personagem
+                barreirasS.animarLoop();
+                persona.animarPersonagem();
+    
+                // Testar colisão entre o personagem e as barreiras
+                if (colisaoTest(persona, barreirasS)) {
+                    console.log("Colisão detectada! Parando o jogo.");
+                    jogoParado = true;  // Sinaliza que o jogo parou
+                    clearInterval(temporizadorJogo);  // Para o loop do jogo
+                    
+                    alert("Game over")
+                }
+            }
+        }, 20);    
+        
+       
+    }
+
+    return {
+        inicioDeJogo
+    }
+}
+
+flappyBird().inicioDeJogo()
+
